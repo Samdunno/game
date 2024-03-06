3bd7807fcbb454f02b3fb3a9db9902c5e0fcd611
crosbieb <crosbieb@bsci.com>
2024-03-05 19:24:29 -0600

    fix cam

--- Camera.java
+++ Camera.java
@@ -1,5 +1,4 @@
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 public class Camera {
@@ -34,12 +33,7 @@ public class Camera {
         //init frame to 0,0 and size of the game panel
         if (lastFrameClip == null) {
             lastFrameClip = g.getClipBounds();
-            // lastFrameClip = new Rectangle(0, 0, panel.getWidth(), panel.getHeight());
         }
-
-        //center buffer in bounds
-        cameraBuffer.setLocation((int) (lastFrameClip.getX() + (lastFrameClip.getWidth() - cameraBuffer.getWidth())/2), 
-        (int)(lastFrameClip.getY() + (lastFrameClip.getHeight() - cameraBuffer.getHeight()) /2) );
 
         //check if our player is within buffer of clip bounds
         if (player.getX() + player.getWidth() > cameraBuffer.getX() + cameraBuffer.getWidth()) {
@@ -47,44 +41,42 @@ public class Camera {
             // moveX = (int) (cameraBuffer.getX() + cameraBuffer.getWidth() - (player.getX() + player.getWidth()));
             moveX = (int)((player.getX() + player.getWidth() - (cameraBuffer.getX() + cameraBuffer.getWidth())) / 10.0);
 
-            
-
         } else if (player.getX() < cameraBuffer.getX() ) {
             moveX = (int) (( player.getX() - cameraBuffer.getX() ) / 10.0);
-
-            
         }
 
         if (player.getY() + player.getHeight() > cameraBuffer.getY() + cameraBuffer.getHeight()) {
             //move cam down
-            // cameraBuffer.setLocation((int)cameraBuffer.getX(), (int)(player.getY() + player.getHeight() - cameraBuffer.getHeight()));
             moveY = (int) (player.getY() + player.getHeight() -(cameraBuffer.getY() + cameraBuffer.getHeight()));
         } else if (player.getY() < cameraBuffer.getY() ) {
             //move cam up
-            // cameraBuffer.setLocation((int)cameraBuffer.getX(), (int)player.getY());
             moveY = (int) (player.getY() - cameraBuffer.getY()  );
         }
-
         
-        // g.translate((int)lastClipBounds.getX(), (int)lastClipBounds.getY());
         if (moveX != 0 || moveY != 0) {
             
-            System.out.println("Move x: " + String.valueOf(moveX) + " Move y: " + String.valueOf(moveY));
-            // lastClipBounds.x = moveX;
-            //moveY = 0;// moveY/2;
+            // System.out.println("Move x: " + String.valueOf(moveX) + " Move y: " + String.valueOf(moveY));
             lastFrameClip.translate(moveX, moveY);
             
         }
 
-        //output frame info TODO:remove me
-        g.drawString(cameraBuffer.toString().replace("java.awt.Rectangle", "Camera Buffer "), 10, 10);
-        g.drawString(lastFrameClip.toString().replace("java.awt.Rectangle", "Cam Clip "), 10, 20);
-        g.drawString("Player X: " + String.valueOf(player.getX() + "Player Y: " + String.valueOf(player.getY())), 10, 40);
-        g.drawString(g.getClipBounds().toString().replace("java.awt.Rectangle", "Frame Clip "), 10, 50);
+        //frame debug
+        // g.drawString(cameraBuffer.toString().replace("java.awt.Rectangle", "Camera Buffer "), 10, 10);
+        // g.drawString(lastFrameClip.toString().replace("java.awt.Rectangle", "Cam Clip "), 10, 20);
+        // g.drawString("Player X: " + String.valueOf(player.getX() + "Player Y: " + String.valueOf(player.getY())), 10, 40);
+        // g.drawString(g.getClipBounds().toString().replace("java.awt.Rectangle", "Frame Clip "), 10, 50);
 
         //inverse for graphics translate
         g.translate(-(int)lastFrameClip.getX(), -(int)lastFrameClip.getY());  
         
+        //center buffer in new location
+        var framloc = g.getClipBounds();
+        cameraBuffer.setLocation((int) (framloc.getX() + (framloc.getWidth() - cameraBuffer.getWidth())/2), 
+            (int)(framloc.getY() + (framloc.getHeight() - cameraBuffer.getHeight()) /2) );
+
+        //camera buffer debug
+        // g.drawRect((int) cameraBuffer.getX(),(int)  cameraBuffer.getY(), (int) cameraBuffer.getWidth(), (int) cameraBuffer.getHeight());
+
     }
 
 
